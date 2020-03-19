8e0a9726ec22283af98b3ef1f22c11d440b1348b
Prince Adeyemi <23622619+LasVegasCoder@users.noreply.github.com>
2020-03-19 13:53:46 -0700

    Create WebController.php

---
+++ WebController.php
@@ -0,0 +1,66 @@
+<?php
+
+	If(!class_exists('WebController')){
+		class WebController{
+			
+			/* private variables */
+			private $_error_message = array();
+			/* public variables */
+			
+			/* utilities */
+			
+			/* constants */
+			
+			public function __Construct(){
+				//echo "Hello contstruct";
+				//$this->run();
+				
+				echo 'Construct <br />';
+				
+				
+				$logindata = array(
+					'username' => 'prince',
+					'userpass' => '12345',
+					'userhash' => 'sjdkidsiwjijfwe'
+				);
+				
+				//$this->debug($logindata);
+			}
+			
+			private function run(){
+				echo 'Hello world!';
+				
+				//exit();
+			}
+			
+			private function login( $login_data = array() ){
+				
+				debug($login_data);
+				
+				
+			}
+			
+			
+			private function register( $register_data = array() ){
+				
+				
+			}
+			
+			private function logout( $logout_data = array() ){
+				
+				
+			}			
+			
+			/*Debug Data */
+			public function debug( $data = array() ){
+				
+				echo '<pre>' . print_r( $data, true )  . '</pre>';
+				exit();
+			}
+			
+		} //end of class WebController
+	}
+
+	
+	new WebController();
+?>
